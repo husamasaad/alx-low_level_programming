f3d7a88b9dbe6b0433127d9a6b659b2d5b5b320c
Husam Asaad <husamibnasaad@gmail.com>
2022-12-08 04:34:38 -0800

    size of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
-	printf("Size of int: %ld byte(s)\n", sizeof(i));
-	printf("Size of long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of float: %ld byte(s)\n", sizeof(f));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 
 	return (0);
 }
