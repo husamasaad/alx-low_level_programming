d9b9d44d77484d7f66795e053dc63daba737709f
Husam Asaad <husamibnasaad@gmail.com>
2022-12-08 04:11:43 -0800

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ *
+ * Descreption: printing using pritnf
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0)
+}
