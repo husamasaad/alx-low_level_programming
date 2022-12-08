5f7509dc23403a850ee8e3adc116ffaf6428c06f
Husam Asaad <husamibnasaad@gmail.com>
2022-12-08 03:35:38 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-	char str[100] ="\"Programming is like building a multilingual puzzle";
+	char str[100] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 
 	return(0);
