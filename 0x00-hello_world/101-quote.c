36a9a18f74b384008f097031f6c30e8a98d932c8
Husam Asaad <husamibnasaad@gmail.com>
2022-12-08 04:50:38 -0800

    print to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print a qoute to standard error
+ *
+ * Return: 1 (error)
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+
+	write(2, quote, 58);
+	return (1);
+}
