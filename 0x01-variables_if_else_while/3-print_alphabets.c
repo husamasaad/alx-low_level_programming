cd950915eb0b56ec5164fa8cd1373203f63b33b0
Husam Asaad <husamibnasaad@gmail.com>
2022-12-09 06:37:53 -0800

    a-zA-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry poin
+ *
+ * Description: prints a-zA-Z
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int l, u;
+	
+	for (l = 'a'; l <= 'z'; ++l)
+		putchar(l);
+	for (u = 'A'; u <= 'Z'; ++u)
+		putchar(u);
+	putchar('\n');
+	return (0);
+}
