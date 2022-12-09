081351741d8e7a3b823b6a3eba9dff419a2916d6
Husam Asaad <husamibnasaad@gmail.com>
2022-12-09 10:29:49 -0800

    numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print all possible digits
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i == 9)
+			break;
+		putchar(',');
+		putchar(0);
+	}
+	putchar('\n');
+	return (0);
+}
