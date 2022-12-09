943bb7eed01afdfc236cbed32052417a25e2fccc
Husam Asaad <husamibnasaad@gmail.com>
2022-12-09 10:18:07 -0800

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints alphabet backwards
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; --i)
+		putcar(i);
+	putchar('\n');
+	return (0);
+}
