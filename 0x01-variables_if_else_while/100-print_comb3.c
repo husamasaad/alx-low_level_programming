e11668122d9e60c17001c5a59e4934bcde8c7564
Husam Asaad <husamibnasaad@gmail.com>
2022-12-09 11:07:51 -0800

    just a try

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * 
+ * Description: advanced task
+ *
+ * Return: 0 (succes)
+ */
+int main()
+{
+	int i, k;
+	int j = 0;
+
+	for (i = 0; i < 10; i++)
+		for (int k = 1; k < 10; k++)
+		{
+			if (k > j)
+			{
+				putchar(i + '0');
+				putchar(k + '0');
+				putchar(',');
+				putchar(' ');
+				j++;
+			}
+		}
+	putchar('\n');
+	return (0);
+}
