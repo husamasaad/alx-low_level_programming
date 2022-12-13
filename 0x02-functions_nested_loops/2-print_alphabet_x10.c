de7372ae19dc1f84184d98472bc78cf5752afe24
Husam Asaad <husamibnasaad@gmail.com>
2022-12-13 03:35:25 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,14 @@
  */
 void print_alphabet_x10(void)
 {
-	int n;
+	int n, i;
 
 	for (n = 0; n <= 10; n++)
 	{
-		print_alphabet();
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
