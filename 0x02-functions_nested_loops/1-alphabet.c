a3eadf1229f8f25203823c7e66f63b75297cbdbb
Husam Asaad <husamibnasaad@gmail.com>
2022-12-13 03:08:36 -0800

    call Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * print_alphabet - call the alphabet
+ *
+ * Descrtiption: prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: 0 Always
+ */
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,5 @@
 /* _putchar */
 int _putchar(char c);
+
+/* call alphabet */
+void print_alphabet(void);
