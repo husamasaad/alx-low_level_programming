68523da2cf84e6a7b71b3aa0130dab492de723c1
Husam Asaad <husamibnasaad@gmail.com>
2022-12-13 11:20:47 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,8 @@
 #include "main.h"
+void empty_digit(num);
+void end_of_line(num);
 /**
- * times_table - prints time table 9 time
+ * print_times_table - prints time table 9 time
  * Return: void
  */
 void print_times_table(int n)
@@ -15,20 +17,15 @@ void print_times_table(int n)
 	{
 		for (num = 0; num < n + 1; num++)
 		{
-			product = (num * row);
-			
+			product = (num * row);		
 			if (product / 100 > 0)
 			{
 				_putchar((product / 100) + '0');
 			}
 			else
 			{
-				if (num > 0)
-				{
-					_putchar(' ');
-				}
+				empty_digit(num);
 			}
-
 			if ((product / 10) > 0 && (product / 10) < 10)
 			{
 				_putchar((product / 10) + '0');
@@ -41,19 +38,29 @@ void print_times_table(int n)
 				}
 				else
 				{
-					if (num > 0)
-					{
-					_putchar(' ');
-					}
+					empty_digit(num);
 				}
 			}
 			_putchar((product % 10) + '0');
-			if (num != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			end_of_line(num);
 		}
 		_putchar('\n');
+	}
+}
+
+void empty_digit(num)
+{
+	if (num > 0)
+	{
+		putchar(' ');
+	}
+}
+
+void end_of_line(num)
+{
+	if (num != n)
+	{
+		_putchar(',');
+		_putchar(' ');
 	}
 }
