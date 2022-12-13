53b6cbd6516fa15351c2c11b005a17e931e3c2fe
Husam Asaad <husamibnasaad@gmail.com>
2022-12-13 09:50:51 -0800

    the og

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,11 +5,17 @@
  */
 void print_to_98(int n)
 {
-	for (n; n <= 98; n++)
+	int i;
+
+	for (i = n; i <= 98; i++)
 	{
-		if (n < 98)
+		if (i < 98)
 		{
-			printf("%d, ", n);
+			printf("%d, ", i);
+		}
+		else
+		{
+			printf("%d", i);
 		}
 	}
 	putchar('\n');
