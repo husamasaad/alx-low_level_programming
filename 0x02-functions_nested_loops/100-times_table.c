6ff60e18d69c6a8e9f9292b138da39fc31367645
Husam Asaad <husamibnasaad@gmail.com>
2022-12-13 10:45:42 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,11 +7,24 @@ void print_times_table(int n)
 {
 	int num, row, product;
 
+	if (n > 15)
+	{
+		break;
+	}
 	for (row = 0; row < n + 1; row++)
 	{
 		for (num = 0; num < n + 1; num++)
 		{
 			product = (num * row);
+			
+			if (product / 100 > 0)
+			{
+				_putchar((product / 100) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
 			if (product / 10 > 0)
 			{
 				_putchar((product / 10) + '0');
@@ -27,7 +40,6 @@ void print_times_table(int n)
 			if (num != n)
 			{
 				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 			}
 		}
