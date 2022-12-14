b5b985232435ec136ace02c906a62f5ba3ba226a
Husam Asaad <husamibnasaad@gmail.com>
2022-12-13 18:47:33 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - print the first 98 fibonacci numbers.
+ * Return: void
+ */
+int main(void)
+{
+	unsigned long count, i, j, k;
+	unsigned long m, n, p, carry;
+
+	int count = 0;
+	i = 0;
+	j = 1;
+	for (count = 0; count < 91; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu", k);
+	}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	n = j % 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
+		p = (m + n) - carry * 1000;
+		k = (i + j) + carry;
+		m = n;
+		n = p;
+		i = j;
+		j = k;
+		if (p >= 100)
+			printf("%lu%lu", k, p);
+		else
+			printf("%lu0%lu", k, p);
+		if (count != 98)
+			printf(", ");
+		count++;
+	}
+	putchar('\n');
+	return (0);
+}
