5f2702f339ec5c457e48c2121a6d93cae1b80a8f
Husam Asaad <husamibnasaad@gmail.com>
2022-12-13 19:48:27 -0800

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,9 +8,7 @@ int main(void)
 	unsigned long n1 = 0, n2 = 1, n3;
 	int i;
 
-	printf("%lu, %lu, ", n1, n2);
-
-	for (i = 2; i <= 98; i++)
+	for (i = 1; i <= 98; i++)
 	{
 		n3 = n1 + n2;
 		if (i < 98)
