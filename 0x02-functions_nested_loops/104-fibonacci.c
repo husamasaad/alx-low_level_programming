c74f85d7d60b7b0c7d55dd852a3711de2376b78c
Husam Asaad <husamibnasaad@gmail.com>
2022-12-13 20:09:10 -0800

    fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,22 +5,32 @@
  */
 int main(void)
 {
-	unsigned long int n1 = 0, n2 = 1, n3;
+	unsigned long int n1 = 1, n2 = 2, nl1, nl2, nl3, nl4;
 	int i;
+
+	printf("%lu", n1);
 
 	for (i = 1; i <= 91; i++)
 	{
-		n3 = n1 + n2;
-		if (i < 91)
-		{
-			printf("%lu, ", n3);
-		}
-		else
-		{
-			printf("%lu\n", n3);
-		}
-		n1 = n2;
-		n2 = n3;
+		printf(", %lu", n2);
+		n2 += n1;
+		n1 = n2 - n1;
 	}
+
+	nl1 = (n1 / 1000000000);
+	nl2 = (n1 % 1000000000);
+	nl3 = (n2 / 1000000000);
+	nl4 = (n2 % 1000000000);
+
+	for (i = 92; i < 98; i++)
+	{
+		printf(", %lu", nl1 + (nl2 / 1000000000));
+		printf("%lu", nl2 % 1000000000);
+		nl3 = nl3 + nl1;
+		nl1 = nl3 - nl1;
+		nl4 = nl4 + nl2;
+		nl2 = nl4 - nl2;
+	}
+	putchar('\n');
 	return (0);
 }
