3ec966bc8bf8289cc00d1b269904c979b2a5fb4d
Husam Asaad <husamibnasaad@gmail.com>
2022-12-19 08:13:05 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#inclued "main.h"
+/**
+ * swap_int - swaps the values
+ * @a: input 1
+ * @b: input 2
+ * Return: integers
+ */
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*a =*b;
+	*b = n;
+}
