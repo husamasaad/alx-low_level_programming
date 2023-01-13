852d968d083b00d54cdd4ae71dfabfe604464434
Husam Asaad <husamibnasaad@gmail.com>
2023-01-13 01:21:42 -0800

    more malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,7 +17,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	a = malloc((sizeof(iny) * (max -min)) + sizeof(int));
+	a = malloc((sizeof(int) * (max - min)) + sizeof(int));
 
 	if (a == NULL)
 	{
