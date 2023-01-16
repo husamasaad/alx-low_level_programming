16e36db9b727689dda818cf5cf3dbb4b481097a8
Husam Asaad <husamibnasaad@gmail.com>
2023-01-16 13:30:38 -0800

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef _HEADER_
+#define _HEADER_
 /**
  * struct dog - Dog attributes
  * @name: The name of the dog.
@@ -22,3 +24,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
